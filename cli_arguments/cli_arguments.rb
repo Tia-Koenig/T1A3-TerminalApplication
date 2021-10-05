722b2d5a31948d31536e06ce99fd1176302013aa
Tia-Koenig <tiakoenig1994@gmail.com>
2021-10-05 22:30:20 +1000

    Refined CLI help message method

--- cli_arguments/cli_arguments.rb
+++
@@ -1,6 +0,0 @@
-require_relative "../utility_methods/display_message.rb"
-require_relative "../messages/help_message.rb"
-
-def help_cli_argument
-    return display_message(help_message())
-end
--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-require_relative "./cli_arguments/cli_arguments.rb"
 require_relative "./utility_methods/display_message.rb"
 require_relative "./messages/welcome_message.rb"
+require_relative "./messages/help_message.rb"
 require_relative "./methods/main_menu.rb"
 
 if ARGV.include?("-h")
-    help_cli_argument()
+    display_message(help_message())
 end
 
 display_message(welcome_message())
